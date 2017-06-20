6bfc1e1507511b3e3e753a110df576a5fd023644
Ghazanfar Mir <GhazanfarMir@gmail.com>
2017-06-20 12:06:10 +0100

    Controller cleanup

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -57,16 +57,5 @@ class LoginController extends Controller
         }
     }
 
-    /**
-     * Get the needed authorization credentials from the request.
-     *
-     * @param  \Illuminate\Http\Request $request
-     * @return array
-     */
-
-    /*protected function credentials(\Illuminate\Http\Request $request)
-    {
-        //return $request->only($this->username(), 'password');
-        return ['email' => $request->{$this->username()}, 'password' => $request->password, 'status' => 1];
-    }*/
+ 
 }
